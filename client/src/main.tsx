d2c99b3692f3e057cdba9f43c18bf55f20c3ac90
Justin Dosaj <jdosaj@vurge.io>
2025-12-09 13:19:10 -0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Testing if I can get this commit to main in replit
+
 createRoot(document.getElementById("root")!).render(<App />);
