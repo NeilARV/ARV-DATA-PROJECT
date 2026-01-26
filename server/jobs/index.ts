acc36150a8561298a7b75ef03edc72cff0fc789c
JustinDosaj <justindosaj@gmail.com>
2026-01-26 10:07:32 -0800

    Add MSA Schedules

--- server/jobs/index.ts
+++ server/jobs/index.ts
@@ -4,7 +4,16 @@ import { CleanCache } from "./cleanCache"
 export function startScheduledJobs() {
     console.log("[CRON] Starting scheduled jobs...")
 
-    // Clean Streetview Cache Every Night at 2:00 AM
-    cron.schedule("0 2 * * *", CleanCache)
+    // Clean Streetview Cache Every Night at 1:00 AM
+    cron.schedule("0 1 * * *", CleanCache)
+
+    // Start San Diego-Chula Vista-Carlsbad, CA property data sync every night at 1:30 AM
+    cron.schedule("30 1 * * *", () => console.log("Starting San Diego Data Sync Job"))
+
+    // Start Los Angeles-Long Beach-Anaheim, CA property data sync every night at 1:45 AM
+    cron.schedule("45 1 * * *", () => console.log("Starting Los Angeles Data Sync Job"))
+
+    // Start Denver-Aurora-Centennial, CO property data sync every night at 2:00 AM
+    cron.schedule("0 2 * * *", () => console.log("Starting Denver Data Sync Job"))
     
 }
