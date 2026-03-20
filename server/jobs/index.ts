da3666394de51023e9d77178bf91c8dea3247a88
JustinDosaj <justindosaj@gmail.com>
2026-03-20 12:07:55 -0700

    Update index.ts

--- server/jobs/index.ts
+++ server/jobs/index.ts
@@ -56,6 +56,7 @@ export function startScheduledJobs() {
     // })
     
     // Consumer: Run at 30 minute mark every hour from 4:30am to 10:30pm — processes all pending market_scan_queue rows
+    // Can adjust time based on whether or not Scanner E is active
     cron.schedule("30 4-22 * * *", runConsumer, {
         timezone: "America/Los_Angeles"
     })
