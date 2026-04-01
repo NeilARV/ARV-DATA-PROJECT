415006870d3830821bb9c46822422833c29ba6ff
JustinDosaj <justindosaj@gmail.com>
2026-04-01 10:55:19 -0700

    Switch Consumer Run Time
    Now runs every 30 minutes between 5am-10pm (earliest time: 5am | latest time: 10:30pm)

--- server/jobs/index.ts
+++ server/jobs/index.ts
@@ -60,13 +60,13 @@ export function startScheduledJobs() {
     
     // Consumer: Run at 30 minute mark every hour from 4:30am to 10:30pm — processes all pending market_scan_queue rows
     // Can adjust time based on whether or not Scanner E is active
-    //if (process.env.NODE_ENV === "production") {
-        cron.schedule("40 * * * *", runConsumer, {
+    if (process.env.NODE_ENV === "production") {
+        cron.schedule("*/30 5-22 * * *", runConsumer, {
             timezone: "America/Los_Angeles"
         })
-   // } else {
-    //    console.log(`[CRON] Consumer skipped — not running in production (NODE_ENV="${process.env.NODE_ENV}")`)
-    //}
+    } else {
+        console.log(`[CRON] Consumer skipped — not running in production (NODE_ENV="${process.env.NODE_ENV}")`)
+    }
 
     
     // =========================================================================
