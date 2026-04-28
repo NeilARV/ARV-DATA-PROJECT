3da000b331fa8916ef41857a8b8e43b2651b45c9
JustinDosaj <justindosaj@gmail.com>
2026-04-28 12:07:28 -0700

    Remove DB Constraint

--- database/migrations/021_signup_auth_flow.sql
+++ database/migrations/021_signup_auth_flow.sql
@@ -1,7 +1,3 @@
 ALTER TABLE users
-    ADD COLUMN county TEXT DEFAULT "San Diego",
-    ADD COLUMN state  VARCHAR(2) DEFAULT "CA";
-
-ALTER TABLE users
-    ADD CONSTRAINT chk_users_county_requires_state
-    CHECK (county IS NULL OR state IS NOT NULL);
+    ADD COLUMN county TEXT DEFAULT 'San Diego',
+    ADD COLUMN state  VARCHAR(2) DEFAULT 'CA';
